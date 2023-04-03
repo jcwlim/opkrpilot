730747d3e98805d99de350bf54d64542b05c4d51
jasonlim <jforum@me.com>
2023-04-03 22:59:00 +0800

    clearing hyundaican values = scc12 assignment

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -142,8 +142,8 @@ def create_scc11(packer, frame, set_speed, lead_visible, scc_live, lead_dist, le
   return packer.make_can_msg("SCC11", 0, values)
 
 def create_scc12(packer, apply_accel, enabled, scc_live, gaspressed, brakepressed, aebcmdact, car_fingerprint, speed, stopping, standstill, radar_recognition, cnt, scc12, radarDisable):
-  if not radarDisable:
-    values = scc12
+  #if not radarDisable:
+  values = scc12
   if not aebcmdact:
     if enabled and car_fingerprint == CAR.NIRO_EV_DE:
       values["ACCMode"] = 2 if gaspressed and (apply_accel > -0.2) else 1
